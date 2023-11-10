94c045eb8c3a4552e6b47aec141bf9d1b9f2df0f
JYLN <14062951+JYLN@users.noreply.github.com>
2023-11-09 22:41:57 -0700

    Loaded data is now sorted. Closes #8

--- src/routes/(app)/shared/+page.server.ts
+++ src/routes/(app)/shared/+page.server.ts
@@ -1,6 +1,7 @@
 export async function load({ locals }) {
   const sharedTimeEntries = locals.pb?.collection('time_entries').getFullList({
     filter: `shared_users ?~ "${locals.user.id}"`,
+    sort: '-start_time',
     expand: 'customer,shared_users'
   });
 
