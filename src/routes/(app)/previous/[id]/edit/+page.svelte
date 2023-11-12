ee5522a9f873e2fd3d45180be2c48c15514ad767
JYLN <14062951+JYLN@users.noreply.github.com>
2023-11-12 16:15:21 -0700

    Add toasts for successful or errored edited time entry submissions

--- src/routes/(app)/previous/[id]/edit/+page.svelte
+++ src/routes/(app)/previous/[id]/edit/+page.svelte
@@ -1,13 +1,41 @@
 <script lang="ts">
   import EditTimeEntryForm from '$lib/components/forms/EditTimeEntryForm.svelte';
+  import { addToast } from '$lib/components/ui/Toaster.svelte';
+  import { editTimeEntrySchema, type EditTimeEntrySchema } from '$lib/schemas/index.js';
   import { convertSelectData } from '$lib/utils.js';
+  import type { FormOptions } from 'formsnap';
 
   export let data;
+
+  const options: FormOptions<EditTimeEntrySchema> = {
+    validators: editTimeEntrySchema,
+    onResult: ({ result }) => {
+      switch (result.type) {
+        case 'success':
+        case 'redirect':
+          addToast({
+            data: {
+              title: 'Success!',
+              description: 'This time entry has been successfully edited!'
+            }
+          });
+          break;
+      }
+    },
+    onError: ({ result }) => {
+      console.log(result);
+      addToast({ data: { title: 'Error!', description: result.error.message } });
+    }
+  };
 </script>
 
 <main class="container mx-auto">
   <h2 class="heading-1">Edit - {data.timeEntry?.name}</h2>
   {#if data.form}
-    <EditTimeEntryForm form={data.form} customers={convertSelectData(data.customerData)} />
+    <EditTimeEntryForm
+      form={data.form}
+      customers={convertSelectData(data.customerData)}
+      {options}
+    />
   {/if}
 </main>
