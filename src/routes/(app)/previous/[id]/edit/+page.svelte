508a04c7042ad8a1babd273b1011dc2ef048c2e8
JYLN <14062951+JYLN@users.noreply.github.com>
2023-11-12 23:26:21 -0700

    Add button to navigate back to previous after entering edit page for time entry, add description paragraph stating start time and end time of entry

--- src/routes/(app)/previous/[id]/edit/+page.svelte
+++ src/routes/(app)/previous/[id]/edit/+page.svelte
@@ -1,9 +1,12 @@
 <script lang="ts">
   import EditTimeEntryForm from '$lib/components/forms/EditTimeEntryForm.svelte';
   import { addToast } from '$lib/components/ui/Toaster.svelte';
+  import { Button } from '$lib/components/ui/button';
   import { editTimeEntrySchema, type EditTimeEntrySchema } from '$lib/schemas/index.js';
   import { convertSelectData } from '$lib/utils.js';
   import type { FormOptions } from 'formsnap';
+  import { Undo2 } from 'lucide-svelte';
+  import moment from 'moment';
 
   export let data;
 
@@ -30,7 +33,19 @@
 </script>
 
 <main class="container mx-auto">
-  <h2 class="heading-1">Edit - {data.timeEntry?.name}</h2>
+  <div class="flex gap-3">
+    <Button variant="ghost" size="icon" href="/previous">
+      <Undo2 />
+    </Button>
+    <div>
+      <h2 class="heading-1 mb-2">Edit - {data.timeEntry?.name}</h2>
+      <p class="mb-4 text-sm text-muted-foreground">
+        Start Time: {moment(data.timeEntry?.start_time).format('MM/DD/YY hh:mma')} • End Time: {moment(
+          data.timeEntry?.end_time
+        ).format('MM/DD/YY hh:mma')}
+      </p>
+    </div>
+  </div>
   {#if data.form}
     <EditTimeEntryForm
       form={data.form}
