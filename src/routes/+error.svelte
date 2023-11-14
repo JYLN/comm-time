ecaaad6aaa75135768fb159b8ee4050bd5df472d
JYLN <14062951+JYLN@users.noreply.github.com>
2023-11-14 13:31:28 -0700

    Add simple error page with button to go back in browser history upon error

---
+++ src/routes/+error.svelte
@@ -0,0 +1,30 @@
+<script lang="ts">
+  import { page } from '$app/stores';
+  import { Button } from '$lib/components/ui/button';
+</script>
+
+<main>
+  <article>
+    <h1>{$page.status}</h1>
+    <p>{$page.error?.message}</p>
+  </article>
+  <Button on:click={() => history.back()}>Go back</Button>
+</main>
+
+<style lang="postcss">
+  main {
+    @apply container mx-auto flex h-screen flex-col items-center justify-center py-5;
+  }
+
+  h1 {
+    @apply mb-3 font-jetmono text-7xl font-bold;
+  }
+
+  p {
+    @apply text-xl font-medium text-muted-foreground;
+  }
+
+  article {
+    @apply grid grid-rows-2 justify-items-center;
+  }
+</style>
