e713560694920b0c757802a3703f4649e8eff4f9
JYLN <14062951+JYLN@users.noreply.github.com>
2023-11-21 12:15:43 -0700

    Initial tabs and form structure

---
+++ src/lib/components/ui/TabCard.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+  import * as Card from '$lib/components/ui/card';
+</script>
+
+<Card.Root class="max-w-[750px]">
+  <Card.Header>
+    <Card.Description>
+      <slot name="description" />
+    </Card.Description>
+  </Card.Header>
+  <Card.Content>
+    <slot name="form" />
+  </Card.Content>
+</Card.Root>
