8d03515361db0504cce7d7ac944f3cfedac58b12
JYLN <14062951+JYLN@users.noreply.github.com>
2023-11-21 11:18:06 -0700

    Initial admin route layout

---
+++ src/routes/(app)/admin/+layout.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+  import { Separator } from '$lib/components/ui/separator';
+</script>
+
+<main class="container mx-auto">
+  <header class="space-y-1">
+    <h2 class="text-3xl font-bold">Admin Settings</h2>
+    <p class="text-sm text-muted-foreground">Manage settings for the overall application.</p>
+  </header>
+  <Separator class="my-6" />
+  <slot />
+</main>
