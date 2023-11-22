885705b72d59939d5ec1bac68a85b82eea456180
JYLN <14062951+JYLN@users.noreply.github.com>
2023-11-22 12:16:48 -0700

    Create render of current customer list next to forms

--- src/lib/components/ui/TabCard.svelte
+++ src/lib/components/ui/TabCard.svelte
@@ -2,7 +2,7 @@
   import * as Card from '$lib/components/ui/card';
 </script>
 
-<Card.Root class="max-w-[750px]">
+<Card.Root>
   <Card.Header>
     <Card.Description>
       <slot name="description" />
