2f4c3bc604e988938a5b92fa2ab74b968bc31dfe
JYLN <14062951+JYLN@users.noreply.github.com>
2023-11-27 17:42:03 -0700

    Make textarea a little bigger

--- src/lib/components/forms/Editor.svelte
+++ src/lib/components/forms/Editor.svelte
@@ -40,6 +40,7 @@
 <div class="flex flex-col gap-2">
   <textarea
     class="flex min-h-[80px] w-full rounded-md border border-input bg-transparent px-3 py-2 text-sm ring-offset-background placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50"
+    rows="8"
     bind:this={textAreaRef}
     bind:value={$editorValue}
     use:actions.textarea
