a3ab87bfa08d59f3443bdd3233270229b7fc1134
JYLN <14062951+JYLN@users.noreply.github.com>
2023-11-29 13:51:45 -0700

    Rename postcss file

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
   import Toaster from '$lib/components/ui/Toaster.svelte';
   import { ModeWatcher } from 'mode-watcher';
-  import '../app.postcss';
+  import '../app.pcss';
 </script>
 
 <Toaster />
