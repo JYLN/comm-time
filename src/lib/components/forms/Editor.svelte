14c0b658b93f14d1c0055f5f6b100b406732194e
JYLN <14062951+JYLN@users.noreply.github.com>
2023-11-28 21:49:09 -0700

    Refactor styles

--- src/lib/components/forms/Editor.svelte
+++ src/lib/components/forms/Editor.svelte
@@ -54,79 +54,89 @@
   }
 </script>
 
-<div class="flex flex-col gap-2">
+<div class="relative">
   <textarea
-    class="flex min-h-[80px] w-full rounded-md border border-input bg-transparent px-3 py-2 text-sm ring-offset-background placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50"
-    rows="8"
+    class="shadcn-textarea"
+    rows="12"
     bind:this={textAreaRef}
     bind:value={$editorValue}
     use:actions.textarea
     {...$attrStore}
   />
 
-  <div class="flex items-center justify-between">
-    <div class="button-container">
-      <Tooltip.Root {...toolTipProps}>
-        <Tooltip.Trigger asChild let:builder>
-          <Button builders={[builder]} variant="ghost" on:click={handleBold}>
-            <Bold class="h-4 w-4" />
-          </Button>
-        </Tooltip.Trigger>
-        <Tooltip.Content>Bold</Tooltip.Content>
-      </Tooltip.Root>
+  <div class="button-container">
+    <Tooltip.Root {...toolTipProps}>
+      <Tooltip.Trigger asChild let:builder>
+        <Button builders={[builder]} variant="outline" class="px-2 py-0" on:click={handleBold}>
+          <Bold class="h-4 w-4" />
+        </Button>
+      </Tooltip.Trigger>
+      <Tooltip.Content>Bold</Tooltip.Content>
+    </Tooltip.Root>
 
-      <Tooltip.Root {...toolTipProps}>
-        <Tooltip.Trigger asChild let:builder>
-          <Button builders={[builder]} variant="ghost" on:click={handleItalic}>
-            <Italic class="h-4 w-4" />
-          </Button>
-        </Tooltip.Trigger>
-        <Tooltip.Content>Italic</Tooltip.Content>
-      </Tooltip.Root>
+    <Tooltip.Root {...toolTipProps}>
+      <Tooltip.Trigger asChild let:builder>
+        <Button builders={[builder]} variant="outline" class="px-2 py-0" on:click={handleItalic}>
+          <Italic class="h-4 w-4" />
+        </Button>
+      </Tooltip.Trigger>
+      <Tooltip.Content>Italic</Tooltip.Content>
+    </Tooltip.Root>
 
-      <Tooltip.Root {...toolTipProps}>
-        <Tooltip.Trigger asChild let:builder>
-          <Button builders={[builder]} variant="ghost" on:click={handleLink}>
-            <Link class="h-4 w-4" />
-          </Button>
-        </Tooltip.Trigger>
-        <Tooltip.Content>Link</Tooltip.Content>
-      </Tooltip.Root>
+    <Tooltip.Root {...toolTipProps}>
+      <Tooltip.Trigger asChild let:builder>
+        <Button builders={[builder]} variant="outline" class="px-2 py-0" on:click={handleLink}>
+          <Link class="h-4 w-4" />
+        </Button>
+      </Tooltip.Trigger>
+      <Tooltip.Content>Link</Tooltip.Content>
+    </Tooltip.Root>
 
-      <Tooltip.Root {...toolTipProps}>
-        <Tooltip.Trigger asChild let:builder>
-          <Button builders={[builder]} variant="ghost" on:click={handleOrderedList}>
-            <ListOrdered class="h-4 w-4" />
-          </Button>
-        </Tooltip.Trigger>
-        <Tooltip.Content>Ordered List</Tooltip.Content>
-      </Tooltip.Root>
+    <Tooltip.Root {...toolTipProps}>
+      <Tooltip.Trigger asChild let:builder>
+        <Button
+          builders={[builder]}
+          variant="outline"
+          class="px-2 py-0"
+          on:click={handleOrderedList}
+        >
+          <ListOrdered class="h-4 w-4" />
+        </Button>
+      </Tooltip.Trigger>
+      <Tooltip.Content>Ordered List</Tooltip.Content>
+    </Tooltip.Root>
 
-      <Tooltip.Root {...toolTipProps}>
-        <Tooltip.Trigger asChild let:builder>
-          <Button builders={[builder]} variant="ghost" on:click={handleUnorderedList}>
-            <List class="h-4 w-4" />
-          </Button>
-        </Tooltip.Trigger>
-        <Tooltip.Content>Unordered List</Tooltip.Content>
-      </Tooltip.Root>
-    </div>
+    <Tooltip.Root {...toolTipProps}>
+      <Tooltip.Trigger asChild let:builder>
+        <Button
+          builders={[builder]}
+          variant="outline"
+          class="px-2 py-0"
+          on:click={handleUnorderedList}
+        >
+          <List class="h-4 w-4" />
+        </Button>
+      </Tooltip.Trigger>
+      <Tooltip.Content>Unordered List</Tooltip.Content>
+    </Tooltip.Root>
 
-    <div class="button-container">
-      <Tooltip.Root {...toolTipProps}>
-        <Tooltip.Trigger asChild let:builder>
-          <Button builders={[builder]} variant="ghost" on:click={handleCopy}>
-            <ClipboardCopy class="h-4 w-4" />
-          </Button>
-        </Tooltip.Trigger>
-        <Tooltip.Content>Copy to clipboard</Tooltip.Content>
-      </Tooltip.Root>
-    </div>
+    <Tooltip.Root {...toolTipProps}>
+      <Tooltip.Trigger asChild let:builder>
+        <Button builders={[builder]} variant="outline" class="px-2 py-0" on:click={handleCopy}>
+          <ClipboardCopy class="h-4 w-4" />
+        </Button>
+      </Tooltip.Trigger>
+      <Tooltip.Content>Copy to clipboard</Tooltip.Content>
+    </Tooltip.Root>
   </div>
 </div>
 
 <style lang="postcss">
   .button-container {
-    @apply w-max rounded-md border px-2 py-1;
+    @apply absolute bottom-0 w-max px-3 py-2;
+  }
+
+  .shadcn-textarea {
+    @apply flex min-h-[80px] w-full rounded-md border border-input bg-transparent px-3 py-2 text-sm ring-offset-background placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50;
   }
 </style>
